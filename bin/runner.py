88e26738b7a23663b97c304b200877b9ad42f17b
Ivan Giuliani <giuliani.v@gmail.com>
2013-12-15 14:40:46 +0100

    Use the RLE loader from the runner to load any on-disk rle files

--- bin/runner.py
+++ bin/runner.py
@@ -3,11 +3,18 @@ import sys
 from golpy import gol
 from golpy import animator
 from golpy import patterns
+from golpy import loader
 
 
 def main(args):
-    start = gol.GameOfLife(patterns.KAREL_P15)
-    animator.curses_animator(start, delay=10)
+    if len(args) > 1:
+        seed = loader.rle_loader(file(args[1]))
+    else:
+        seed = patterns.KAREL_P15  # or any other pattern
+
+    game = gol.GameOfLife(seed)
+    animator.curses_animator(game, delay=10)
+
     return False
 
 
